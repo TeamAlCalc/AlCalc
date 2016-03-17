cb40fba0d9089f324fe00ecc23c788328380f2e1
Ethan Ingalls <ei8728@uncw.edu>
2016-03-16 21:22:27 -0400

    Added Calculate View Controller for Katie to do calculations on.

---
+++ AlCalc/CalculateViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AlCalc
+//
+//  Created by Ethan on 3/16/16.
+//  Copyright © 2016 Team AlCalc. All rights reserved.
+//
+
+import UIKit
+
+class CalculateViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
