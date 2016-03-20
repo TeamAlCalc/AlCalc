fa2affdf004e14e71a5b83f89d746cc7cc888e6c
Young, Kennon Lucas <kly2888@ci1017-08.local>
2016-03-20 17:04:51 -0400

    Kennon:commit

---
+++ AlCalc/InitialResponseViewControllerH.swift
@@ -0,0 +1,22 @@
+//
+//  InitialResponseViewControllerH.swift
+//  AlCalc
+//
+//  Created by Young, Kennon Lucas on 3/20/16.
+//  Copyright © 2016 Team AlCalc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class InitialResponseViewControllerH : UIViewController {
+    
+    
+    @IBOutlet weak var Label: UILabel!
+    
+    var LabelText = String()
+    
+    override func viewDidLoad() {
+        <#code#>
+    }
+}
