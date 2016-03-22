d699eaa8afa89b1b39475157acc792a48d67fce3
Ethan Ingalls <ei8728@uncw.edu>
2016-03-22 17:49:29 -0400

    Updated ViewController dimensions, added dock to HomepageViewController

--- AlCalc/HomepageViewController.swift
+++ AlCalc/HomepageViewController.swift
@@ -17,7 +17,7 @@ class HomepageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        homepageHeaderLabel.text = toPass
+
         
     }
     
