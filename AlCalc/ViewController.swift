eb6520f292a48f0bb7248067dee417199b603898
Ethan Ingalls <ei8728@uncw.edu>
2016-03-25 14:02:43 -0400

    Updated main page to be age verification instead of login page.

--- AlCalc/ViewController.swift
+++ AlCalc/ViewController.swift
@@ -11,15 +11,26 @@ import UIKit
 class ViewController: UIViewController {
 
     
-    
-    @IBOutlet weak var usernameText: UITextField!
-    @IBOutlet weak var passwordText: UITextField!
-    @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var headerLabel: UILabel!
+    
+    @IBOutlet weak var ofAgeButton: UIButton!
+    
+    @IBOutlet weak var tosButton: UIButton!
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    
+    var dateOfBirth: String!
+    
+    var calendar : NSCalendar = NSCalendar.currentCalendar()
+    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        datePicker.addTarget(self, action:nil, forControlEvents: UIControlEvents.ValueChanged)
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,13 +39,47 @@ class ViewController: UIViewController {
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // let yourNextViewController = (segue.destinationViewController as! HomepageViewController)
-        // yourNextViewController.toPass = usernameText.text
+         let yourNextViewController = (segue.destinationViewController as! HomepageViewController)
+         yourNextViewController.toPass = dateOfBirth
         
     }
     
+    @IBAction func ofAgeConfirm(sender: UIButton) {
+ 
+        let date = datePicker.date
+        let now = NSDate()
+
+        let ageComponents = calendar.components(.Year,
+            fromDate: date,
+            toDate: now,
+            options: [])
+        
+        if ageComponents.year >= 21 {
+        
+            let dateFormatter = NSDateFormatter()
+            dateFormatter.dateStyle = .MediumStyle
+            
+            dateOfBirth = dateFormatter.stringFromDate(date)
+            
+            let alert = UIAlertController(title: "Confirm", message: "By clicking accept I agree that I am the age previously specified and I agree to the Terms of Service.", preferredStyle: UIAlertControllerStyle.Alert)
+             alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Default, handler: nil))
+            alert.addAction(UIAlertAction(title: "I Agree", style: UIAlertActionStyle.Default, handler: { (action: UIAlertAction!) in
+                self.performSegueWithIdentifier("Homepage", sender: nil)
+            }))
+
+            self.presentViewController(alert, animated: true, completion: nil)
+        } else {
+            let alert = UIAlertController(title: "Error", message: "You must be 21 years of age or older to use this application.", preferredStyle: UIAlertControllerStyle.Alert)
+            
+            alert.addAction(UIAlertAction(title: "Okay", style: UIAlertActionStyle.Default, handler: nil))
+            
+            self.presentViewController(alert, animated: true, completion: nil)
+            
+        }
+        
+    }
     
-    @IBAction func login(sender: UIButton) {
+    /*@IBAction func login(sender: UIButton) {
         
         if usernameText.text == "Ethan"{
             print("Log in successfully")
@@ -47,8 +92,8 @@ class ViewController: UIViewController {
             alert.addAction(UIAlertAction(title: "Okay", style: UIAlertActionStyle.Default, handler: nil))
             self.presentViewController(alert, animated: true, completion: nil)
         }
-
-    }
+    
+    }*/
 
 }
 
