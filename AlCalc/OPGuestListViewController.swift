c47c47b36d779a9a365761946e9fab52e970abae
Young, Kennon Lucas <kly2888@ci1017-08.local>
2016-04-19 21:18:18 -0400

    kennonHEY

---
+++ AlCalc/OPGuestListViewController.swift
@@ -0,0 +1,24 @@
+//
+//  OPGuestListViewController.swift
+//  AlCalc
+//
+//  Created by Young, Kennon Lucas on 4/19/16.
+//  Copyright © 2016 Team AlCalc. All rights reserved.
+//
+
+import UIKit
+
+class OPGuestListViewController : UIViewController{
+    
+    @IBOutlet weak var OPGuestList: UITableView!
+    
+    @IBOutlet weak var navBar: UINavigationBar!
+
+    @IBOutlet weak var price: UILabel!
+
+    @IBOutlet weak var can: UILabel!
+
+    @IBOutlet weak var ABV: UILabel!
+
+    @IBOutlet weak var percentage: UILabel!
+}
